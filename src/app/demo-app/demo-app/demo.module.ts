772d487f4d168ca39d116dfd2140afbc234d8dbf
Thompson Filgueiras <tigredonorte3@gmail.com>
2018-03-27 22:51:51 -0300

    rm comments

--- src/app/demo-app/demo-app/demo.module.ts
+++ src/app/demo-app/demo-app/demo.module.ts
@@ -15,31 +15,6 @@ import { DemoEasyFormsModule } from '../components/demo-easy-forms.module';
 import { DemoAppComponent, HomeComponent } from './demo-app.component';
 import { DEMO_APP_ROUTES } from './routes';
 
-// import { MatAutocompleteModule } from '@angular/material/autocomplete';
-// import { MatButtonToggleModule } from '@angular/material/button-toggle';
-// import { MatCheckboxModule } from '@angular/material/checkbox';
-// import { MatChipsModule } from '@angular/material/chips';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatDividerModule } from '@angular/material/divider';
-// import { MatExpansionModule } from '@angular/material/expansion';
-// import { MatGridListModule } from '@angular/material/grid-list';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatNativeDateModule } from '@angular/material/core';
-// import { MatPaginatorModule } from '@angular/material/paginator';
-// import { MatProgressBarModule } from '@angular/material/progress-bar';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-// import { MatRadioModule } from '@angular/material/radio';
-// import { MatRippleModule } from '@angular/material/core';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatSliderModule } from '@angular/material/slider';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { MatSortModule } from '@angular/material/sort';
-// import { MatStepperModule } from '@angular/material/stepper';
-// import { MatTableModule } from '@angular/material/table';
-// import { MatTabsModule } from '@angular/material/tabs';
-// import { MatTooltipModule } from '@angular/material/tooltip';
-
 @NgModule({
   imports: [
     CommonModule,
