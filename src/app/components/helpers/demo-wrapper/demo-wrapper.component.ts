fd2222cb721a7cb47ad7c550c8bf0fcb20704230
tigredonorte <tigredonorte3@gmail.com>
2018-04-18 09:18:43 -0300

    copy to clipboard

--- src/app/components/helpers/demo-wrapper/demo-wrapper.component.ts
+++ src/app/components/helpers/demo-wrapper/demo-wrapper.component.ts
@@ -9,6 +9,7 @@ export class DemoWrapperComponent implements OnChanges {
   @Input() fieldName = '';
   @Input() options = {};
   _fieldName = '';
+  copyText = '';
   ngModelExample = '';
   ReactiveModuleExample = '';
   InOutExample = '';
@@ -17,17 +18,17 @@ export class DemoWrapperComponent implements OnChanges {
   ngOnChanges() {
     this._fieldName = this.toTitleCase(this.fieldName);
     this.ngModelExample = this.getExample({
-      '[(ngModel)]' : 'myModel',
-      'placeholder' : `my${this._fieldName}`,
+      '[(ngModel)]': 'myModel',
+      placeholder: `my${this._fieldName}`
     });
     this.ReactiveModuleExample = this.getExample({
-      '[formControl]' : 'control',
-      'placeholder' : `my${this._fieldName}`,
+      '[formControl]': 'control',
+      placeholder: `my${this._fieldName}`
     });
     this.InOutExample = this.getExample({
-      '[inputValue]' : 'myInput',
-      '(valueChanged)' : 'myOutput=$event',
-      'placeholder' : `my${this._fieldName}`,
+      '[inputValue]': 'myInput',
+      '(valueChanged)': 'myOutput=$event',
+      placeholder: `my${this._fieldName}`
     });
   }
 
@@ -54,5 +55,24 @@ export class DemoWrapperComponent implements OnChanges {
     return str.replace(/\w\S*/g, function(txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
+  }
+
+  copyToClipboard(id: string) {
+    try {
+      const elem = document.getElementById(id);
+      const selection = window.getSelection();    // Save the selection.
+      const range = document.createRange();
+      range.selectNodeContents(elem);
+      selection.removeAllRanges();          // Remove all ranges from the selection.
+      selection.addRange(range);            // Add the new range.
+      const successful = document.execCommand('copy');
+      this.copyText = successful ? 'text copied to clipboard!' : 'text not copied!';
+      selection.removeAllRanges();
+    } catch (err) {
+      this.copyText = 'Oops, unable to copy';
+    }
+    setTimeout(() => {
+      this.copyText = '';
+    }, 3000);
   }
 }
